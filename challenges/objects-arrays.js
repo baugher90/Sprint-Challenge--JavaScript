9af666c70984c72df5b01ae6fe1a387d1ab82f13
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-01-25 16:24:46 -0500

    classes.js MVPs complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -67,7 +67,10 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 
 const universities = [];
-console.log(universities)
+graduates.forEach(function(obj){
+  universities.push(obj.university);
+});
+console.log(universities.sort())
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -76,11 +79,19 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+graduates.forEach(function(obj){
+  contactInfo.push(obj.first_name)+contactInfo.push(obj.email);
+});
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+
+graduates.includes(graduates,'uni'){
+  uni.push()
+}
+
 console.log(uni);
 
 
@@ -139,4 +150,3 @@ console.log(populationTotal);
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
 
 */
-
