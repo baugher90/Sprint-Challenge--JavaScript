ec19b08cb3ef65c7355077115ea607880e340f68
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-01-25 14:04:18 -0500

    classes.js MVPs complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,28 +7,44 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
-
+const tyrannosaurus = {
+  diet: `carnivorous`,
+  weight: `7000kg`,
+  length: `12m`,
+  timePeriod: `Late Cretaceious`,
+  roar: `RAWERSRARARWERSARARARRRR!`
+}
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+const stegosaurus = {
+  diet: `herbivorous`,
+  weight: `2000kg`,
+  length: `9m`,
+  timePeriod: `Late Jurassic`
+}
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
-
+const velociraptor = {
+  diet: `carnivorous`,
+  weight: `15kg`,
+  length: `1.8m`,
+  timePeriod: `Late Cretaceious`
+}
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.timePeriod);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.roar);
 
 
 // ==== Arrays ====
@@ -49,6 +65,7 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
+
 const universities = [];
 console.log(universities)
 
